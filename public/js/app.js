54f52213429ef8aef69148b0a7723631f72482d5
Giveitaworrell <71277101+Giveitaworrell@users.noreply.github.com>
2021-09-15 00:25:12 -0400

    Anchor Point

--- public/js/app.js
+++ public/js/app.js
@@ -127,7 +127,7 @@ function initMap() {
       markerOptions: {
         icon: ({
           url:"http://maps.google.com/mapfiles/dir_0.png",
-          anchor: new google.maps.Point(0,-15),
+          anchor: new google.maps.Point(25, 20),
         }),
         draggable: true,
       },
